43c95c72c8247f0597c0fc3d1b360e6e7220756a
Alise <mavlidam316@hotmail.com>
2024-05-02 20:16:08 +0200

    User Login implementation

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,33 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import check_password_hash
+from ..models import User
 from . import auth_bp
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return redirect(url_for('main.index'))
-    
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(Username=username).first()
+
+        if user and check_password_hash(user.Password, password):
+            login_user(user)
+            flash('Innlogging vellykket!', 'success')
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('auth.home'))
+        else:
+            flash('Ugyldig brukernavn eller passord!', 'danger')
+
     return render_template('auth/login.html')
+
+@auth_bp.route('/logout')
+def logout():
+    logout_user()
+    flash('Du er logget ut!', 'success')
+    return redirect(url_for('auth.login'))
+
+@auth_bp.route('/home', methods=['GET', 'POST'])
+@login_required
+def home():
+    return render_template('main/home.html', user=current_user)
--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
-import requests
+from flask_login import login_required, current_user
 from . import main
 
 @main.route('/')
 def index():
-    return render_template('/main/home.html')
+    return render_template('/auth/login.html')
 
+@main.route('/home')
+@login_required
+def home():
+    return render_template('/main/home.html', user=current_user)
 
 @main.route('/robot_status/<isar_id>')
+@login_required
 def robot_status(isar_id):
     return render_template('/robot_status/robot_status.html', isar_id=isar_id)

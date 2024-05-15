4de7fc50b81ad73e4940b43df9d09964807650e2
Dennis Mai <dennis.mai@hotmail.com>
2024-05-15 12:26:17 +0200

    Added SSL support

--- manage.py
+++ manage.py
@@ -1,6 +1,11 @@
+import os
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+    ssl_context = (
+        os.getenv('SSL_CERT_PATH'), 
+        os.getenv('SSL_KEY_PATH')
+    )
+    socketio.run(app, debug=True, port=5000, ssl_context=ssl_context)

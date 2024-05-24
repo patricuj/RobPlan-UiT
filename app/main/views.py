db8096785ea454c6ef5e644c9033e919087030fa
Dennis Mai <dennis.mai@hotmail.com>
2024-05-24 10:04:27 +0200

    Calendar in dashboard

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 from . import main
+from ..models import Mission
 
 @main.route('/')
 def index():
@@ -9,4 +10,5 @@ def index():
 @main.route('/home')
 @login_required
 def home():
-    return render_template('/main/home.html', user=current_user)
+    missions = Mission.query.all()
+    return render_template('/main/home.html', user=current_user, missions=missions)
